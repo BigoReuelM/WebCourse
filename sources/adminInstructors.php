53c541bbd666f3754f9c55cafcc72b5e311c4246
Reuel <2121855@slu.edu.ph>
2018-05-02 19:31:09 +0800

    instructor display

--- sources/adminInstructors.php
+++ sources/adminInstructors.php
@@ -6,7 +6,46 @@
     <!-- Activity Area Start -->
     <section class="activity-area section">
         <div class="container">
-            <h1>Instructors</h1>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="well">
+                        <h1 class="pull-left">Instructors</h1>
+                        <?php include('process/getInstructors.php'); ?>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <p class="text-center">Add Instructor</p>
+                        </div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" method="POST" action="fragments/addInstructor.php">
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label" for="instructorFname">First Name:</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="instructorFname">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label" for="instructorMname">Middle Name:</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="instructorMname">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label" for="instructorLname">Last Name:</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="instructorLname">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="panel-footer">
+                            <button class="btn btn-primary">Add</button>
+                        </div>
+                    </div>        
+                </div>
+            </div>
         </div>
     </section>
     <!-- Courses Section End -->
