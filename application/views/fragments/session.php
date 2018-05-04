4e4648bc8a4f3190d25a396435912ddd5b830c7d
Reuel <2121855@slu.edu.ph>
2018-05-04 22:50:08 +0800

    removed old files

--- application/views/fragments/session.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  $userLogedin = false;
-  $userRole = "none";
-  if (isset($_SESSION['userID'])) {
-    $userLogedin = true;
-    $userRole = $_SESSION['userType'];
-    $userName = $_SESSION['firstName'] . " " . $_SESSION['middleName'] . " " . $_SESSION['lastName'];
-  }
-?> 
--- application/views/jspMainPage.php
+++
@@ -1,9 +0,0 @@
-
-
-    <!-- Activity Area Start -->
-    <section class="activity-area section">
-        <div class="container">
-            <h1>JSP</h1>
-        </div>
-    </section>
-    <!-- Courses Section End -->
