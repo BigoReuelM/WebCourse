7ba1e7578bd4f9a8d010716f4268c599f9035b4f
Lemuel Estacio <estacio.lemuel@gmail.com>
2018-05-10 12:15:59 +0800

    initial sources

--- application/views/sources.php
+++ application/views/sources.php
@@ -3,7 +3,20 @@
     <section class="activity-area section">
         <div class="container">
             <h1>Sources</h1>
+        
+        <a href = "#" >Java Web Servlets</a>
+        <a href = "#" > <p>Java Server Pages (JSP)</p></a>
+        <a href = "#" > <p> PHP </p></a>
+        <a href = "#" > <p> Node.js </p></a>
+        <a href = "#" > <p>Web Application< Security</p></a>
+
+       <h1> Tools </h1>
+        <a href = "#" >Code igniter</a>
+         <a href = "#" >Php</a>
+
         </div>
+
+
     </section>
     <!-- Courses Section End -->
 
