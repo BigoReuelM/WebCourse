390f33d0f50a3ff9d424186d1f8b292fe27b707d
Reuel <2121855@slu.edu.ph>
2018-05-11 19:35:11 +0800

    selection of topic in adding of activities

--- application/views/activities.php
+++ application/views/activities.php
@@ -4,26 +4,39 @@
 
         <div class="container">
             <div class="row">
-                <h1 align="center">Add Activity</h1>
+                <h1 align="center">Manage Activities</h1>
             </div>
-            <div class="row" align="center">
-                <div class="form-group">
-                    <label for="questionNumber">Number of Questions: </label>
-                    <input type="number" id="questionNumber">
-                </div>
-                <button class="btn-primary" id="activityButton">Go</button>
-            </div>
+                        
             <br>
-            <div class="row">
-                
-            </div>
             <div class="row">
 
                 <div class="panel-info well">
-                    <div class="panel-heading"></div>
+                    <div class="panel-heading" align="center">
+                        <h1>Create Activity</h1>
+                        <div class="row" align="center">
+                            <div class="form-group">
+                                <label for="questionNumber">Number of Questions: </label>
+                                <input type="number" id="questionNumber">
+                            </div>
+                            <button class="btn-primary" id="activityButton">Go</button>
+                        </div>
+                    </div>
                     <div class="panel-body">
                         <form action="" id="addQuestionForm" class="form-horizontal">
-                    
+                            <div class="form-group">
+                                <label for="topicName" class="col-lg-3 control-label">Select Topic:</label>
+                                <div class="col-lg-9">
+                                    <select class="form-control">
+                                        <option selected disabled hidden>Choose Topic for This Activity...</option>
+                                        <option value="servlets">Java Web Servlets</option>
+                                        <option value="jsp">Java Server Pages</option>
+                                        <option value="php">PHP</option>
+                                        <option value="node">Node.js</option>
+                                        <option value="webSecurity">Web Application Security</option>
+                                    </select>
+
+                                </div>
+                            </div>
                     
                         </form>
                     </div>
