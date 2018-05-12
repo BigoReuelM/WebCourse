ab065bb2b6cb31f03d16d8ef95099818f4d1f739
Lemuel Estacio <estacio.lemuel@gmail.com>
2018-05-12 11:47:02 +0800

    final source page

--- application/views/sources.php
+++ application/views/sources.php
@@ -4,15 +4,12 @@
         <div class="container">
             <h1>Sources</h1>
         
-        <a href = "#" >Java Web Servlets</a>
-        <a href = "#" > <p>Java Server Pages (JSP)</p></a>
-        <a href = "#" > <p> PHP </p></a>
-        <a href = "#" > <p> Node.js </p></a>
-        <a href = "#" > <p>Web Application< Security</p></a>
+        <a href = "https://docs.oracle.com/javaee/6/tutorial/doc/bnafd.html" target="_blank">Java Web Servlets</a>
+        <a href = "https://docs.oracle.com/javaee/5/tutorial/doc/bnajo.html" target="_blank" > <p>Java Server Pages (JSP)</p></a>
+        <a href = "http://php.net/docs.php" target="_blank"> <p> PHP </p></a>
+        <a href = "https://nodejs.org/en/docs/" target="_blank"> <p> Node.js </p></a>
+        <a href = "https://www.owasp.org/index.php/Top_10-2017_Top_10" target="_blank"> <p>Web Application Security</p></a>
 
-       <h1> Tools </h1>
-        <a href = "#" >Code igniter</a>
-         <a href = "#" >Php</a>
 
         </div>
 
